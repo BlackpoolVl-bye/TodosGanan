5574d0f24418ccb654253d6cee07791424422d6b
Aaron Barcenas <114181941+Astharmin@users.noreply.github.com>
2025-03-17 23:00:30 -0400

    Update Conexion.js

--- JS/Conexion.js
+++ JS/Conexion.js
@@ -2,8 +2,8 @@ const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
     host: "localhost",
-    user: "Admin",
-    password: "1234Local.",
+    user: "root",
+    password: "admin",
     database: "bd_todosganan",
     waitForConnections: true,
     connectionLimit: 10,
